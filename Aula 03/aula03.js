1b1f7753e7ac9a7be5afb175493bdd60f67c7b20
Walace Ramos <contatowalace.dev@gmail.com>
2024-03-15 10:33:05 -0300

    aula 03

--- Aula 03/aula03.js
+++ Aula 03/aula03.js
@@ -1,1 +1,17 @@
-/* Dicas para node js */
+/* Tipo de variaves */
+
+"use strict"
+
+function tete(){
+    let nome ="Walace"
+
+    if (true){
+        console.log("dentro do if de teste: " + nome)
+    }
+    console.log("Dentro do teste: " + nome)
+
+}
+
+teste()
+
+console.log("Fora do teste: " + nome)
