db4d6e4b9730d4006acf5e7cc8ca3f16b296a4ae
Walace Ramos <contatowalace.dev@gmail.com>
2024-03-15 20:57:03 -0300

    aula 04

--- Aula 04/aula04.js
+++ Aula 04/aula04.js
@@ -6,3 +6,10 @@ num2=10
 res=num1+num2
 
 console.log(res)
+
+/*
+Para obtermos um valor com +1 para uma variável, utilizamos ++
+e para reduzir em um, utilizamos --
+
+Semelhanças, +=1 ou mais += 5 ou mais +=10
+*/
