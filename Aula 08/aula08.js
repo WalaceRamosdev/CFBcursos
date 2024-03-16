bbfd3f6c45ddd5816c738a33e512c09444e219b8
Walace Ramos <contatowalace.dev@gmail.com>
2024-03-15 22:10:53 -0300

    aula 08

---
+++ Aula 08/aula08.js
@@ -0,0 +1,33 @@
+/*
+
+Operadores Bitwise são usados para operar os bits de cada elemento
+
+*/
+
+let n1 = 10
+let n2 = 11
+
+let res = n1 & n2
+
+console.log(res)
+
+/*
+
+Nó código acima, o resultado exibido na console é = 10.
+
+Para entender o que aconteceu :
+    Foi utilizado o operador BitWise (&) que retorna aonde houver equivalência o valor de número 0 ou 1.
+*/
+
+let n3 = 10
+let n4 = 11
+
+let res1 = n3 | n4
+
+console.log(res)
+
+/*
+
+O deslocamento de bits consiste em mover para direita ou esquerda...ou seja, movendo para esquerdo, dobramos o valor atribuido, movendo para a direita, dividimos por dois o valor atribuido
+
+*/
