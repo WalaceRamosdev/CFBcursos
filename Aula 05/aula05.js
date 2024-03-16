b9428efc3387c8a52288055cfb185e36e2b369d3
Walace Ramos <contatowalace.dev@gmail.com>
2024-03-15 21:05:53 -0300

    aula 05

---
+++ Aula 05/aula05.js
@@ -0,0 +1,17 @@
+/*
+Operadores relacionais são comparações que são os seguintes simbolos:
+
+> - maior
+>= - maior e igual
+< - menor
+<= - menor e igual
+== - igual
+!= - diferente
+
+*/
+
+let num1= 10
+let num2= 5
+let num3= 10
+
+console.log(num1 >= num3)
